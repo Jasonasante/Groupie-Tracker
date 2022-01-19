2d907b698108dd1ca090cf7dbc34c6d404c0f0ca
jasonasante <jason.asante@outlook.com>
2022-01-19 20:04:38 +0000

    receive Artist API

--- main.go
+++ main.go
@@ -1,30 +1,63 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
-type Artist struct{
-	Name string json:"id"
-	Image []byte
-	Members string
-	DoA string
-	DoFA string
+type Relation struct {
+	Artist
 }
 
-type Location struct{
+// type Artist struct {
+// 	Id              uint   `json:"id"`
+// 	Name            string `json:"name"`
+// 	Image           string `json:"image"`
+// 	Members         string `json:"members"`
+// 	CreationDate    uint   `json:"creationDate"`
+// 	FirstAlbum      string `json:"firstAlbum"`
+// 	ConcertLocations string `json:"locations"`
+// 	ConcertDates    string `json:"concertDates"`
+// 	Relations string
+// }
 
+type Artist struct {
+	Id               uint
+	Name             string
+	Image            string
+	Members          string
+	CreationDate     uint
+	FirstAlbum       string
+	ConcertLocations string
+	ConcertDates     string
+	Relations        string
 }
 
-func homePage (w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "Homepage")
-}
+var Relationship []Relation
+
+// func homePage(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, "Homepage")
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	json.NewEncoder(w).Encode(Relationship)
+// }
 
 func main() {
-	fmt.Println("Starting Server at Port 8080")
-	fmt.Println("now open a broswer and enter: localhost:8080 into the URL")
-	http.HandleFunc("/", homePage)
-	http.ListenAndServe(":8080", nil)
+	artist, _ := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	artistData, _ := ioutil.ReadAll(artist.Body)
+	// fmt.Println(string(artistData))
+	// We made type Artist a slice because our api is an array of json data
+	var artistInfo []Artist
+	json.Unmarshal(artistData, &artistInfo)
+	for _, name := range artistInfo {
+		fmt.Println(name.Name)
+	}
+	// fmt.Println((jartist))
+
+	// fmt.Println("Starting Server at Port 8080")
+	// fmt.Println("now open a broswer and enter: localhost:8080 into the URL")
+	// http.HandleFunc("/", homePage)
+	// http.ListenAndServe(":8080", nil)
 }
