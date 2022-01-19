df14bd06aff1ec7413d753a36fb96f5368692e97
jasonasante <jason.asante@outlook.com>
2022-01-19 22:03:55 +0000

    transfer API data onto webpage but title repeats due to repetition of template.html from loop

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -11,53 +13,81 @@ type Relation struct {
 	Artist
 }
 
-// type Artist struct {
-// 	Id              uint   `json:"id"`
-// 	Name            string `json:"name"`
-// 	Image           string `json:"image"`
-// 	Members         string `json:"members"`
-// 	CreationDate    uint   `json:"creationDate"`
-// 	FirstAlbum      string `json:"firstAlbum"`
-// 	ConcertLocations string `json:"locations"`
-// 	ConcertDates    string `json:"concertDates"`
-// 	Relations string
-// }
-
-type Artist struct {
-	Id               uint
-	Name             string
-	Image            string
-	Members          string
-	CreationDate     uint
-	FirstAlbum       string
-	ConcertLocations string
-	ConcertDates     string
-	Relations        string
+type ArtistSlice struct {
+	ArtistSl []Artist
 }
 
-var Relationship []Relation
+type Artist struct {
+	Id               uint     `json:"id"`
+	Name             string   `json:"name"`
+	Image            string   `json:"image"`
+	Members          []string `json:"members"`
+	CreationDate     uint     `json:"creationDate"`
+	FirstAlbum       string   `json:"firstAlbum"`
+	ConcertLocations string   `json:"locations"`
+	ConcertDates     string   `json:"concertDates"`
+	Relations        string   `json:"relations"`
+	MemNames         string
+	artistInf []Artist
+}
 
-// func homePage(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprint(w, "Homepage")
+var (
+	Relationship []Relation
+	SliceArtist  []Artist
+)
 
-// 	w.Header().Set("Content-Type", "application/json")
-// 	json.NewEncoder(w).Encode(Relationship)
-// }
-
-func main() {
+func homePage(w http.ResponseWriter, r *http.Request) {
 	artist, _ := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	artistData, _ := ioutil.ReadAll(artist.Body)
-	// fmt.Println(string(artistData))
 	// We made type Artist a slice because our api is an array of json data
 	var artistInfo []Artist
+	var artistId uint
+	var artistName string
+	var artistImage string
+	var artistMembers []string
+	var artistCreationDate uint
+	var artistFirstAlbum string
+	var artistConcertLocations string
+	var artistConcertDates string
+	var artistRelations string
+	var members string
+	// var ArtistS ArtistSlice
+
 	json.Unmarshal(artistData, &artistInfo)
-	for _, name := range artistInfo {
-		fmt.Println(name.Name)
+	for _, data := range artistInfo {
+		SliceArtist = append(SliceArtist, data)
+		artistId = data.Id
+		artistName = data.Name
+		artistImage = data.Image
+		artistMembers = data.Members
+		artistCreationDate = data.CreationDate
+		artistFirstAlbum = data.FirstAlbum
+		artistConcertLocations = data.ConcertLocations
+		artistConcertDates = data.ConcertDates
+		artistRelations = data.Relations
+		for i, mem := range artistMembers {
+			if i < len(artistMembers)-1 {
+				members += mem
+				members += "\n"
+			} else {
+				members += mem
+			}
+		}
+		t, err := template.ParseFiles("template.html")
+		if err != nil {
+			log.Fatal()
+		}
+		t.Execute(w, Artist{Id: artistId, Name: artistName, Image: artistImage, Members: artistMembers, CreationDate: artistCreationDate, FirstAlbum: artistFirstAlbum, ConcertLocations: artistConcertLocations, ConcertDates: artistConcertDates, Relations: artistRelations, MemNames: members})
 	}
+	fmt.Println(artistInfo[1])
+	fmt.Println(len(artistInfo))
+}
+
+func main() {
 	// fmt.Println((jartist))
 
-	// fmt.Println("Starting Server at Port 8080")
-	// fmt.Println("now open a broswer and enter: localhost:8080 into the URL")
-	// http.HandleFunc("/", homePage)
-	// http.ListenAndServe(":8080", nil)
+	fmt.Println("Starting Server at Port 8080")
+	fmt.Println("now open a broswer and enter: localhost:8080 into the URL")
+	http.HandleFunc("/", homePage)
+	http.ListenAndServe(":8080", nil)
 }
